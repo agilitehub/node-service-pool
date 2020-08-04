5696c3b412b125e7babf5ab87fbd349896a2fa85
John Jardin <john@agilite.io>
2020-08-04 09:21:46 +0200

    Removed manual use of EnumsTypeDetect and other small adhoc tweaks

--- test/001-mongoose.js
+++ test/001-mongoose.js
@@ -1,19 +1,18 @@
 'use strict'
 
-require('dotenv').config()
 const mongoose = require('mongoose')
 const expect = require('chai').expect
-const typeDetect = require('type-detect')
+const TypeDetect = require('agilite-utils/type-detect')
+const EnumsTypeDetect = require('agilite-utils/enums-type-detect')
 
-const servicePool = require('../lib/ServicePool')
-const enumsTypeDetect = require('../lib/EnumsTypeDetect')
+const ServicePool = require('../lib/ServicePool')
 
-let id1 = 'id111'
-let id2 = 'id222'
-let id3 = 'id333'
+const id1 = 'id111'
+const id2 = 'id222'
+const id3 = 'id333'
 
 const config = { max: 2 }
-const pool = new servicePool(config)
+const pool = new ServicePool(config)
 
 const serviceEntry1 = {
   id: id1,
@@ -59,91 +58,91 @@ const serviceEntry3 = {
   }
 }
 
-describe ('Mongoose', () => {
+describe('Mongoose', () => {
   let service1 = null
   let service2 = null
   let service3 = null
 
-  it ('Check Max Prop is valid', (done) => {
+  it('Check Max Prop is valid', (done) => {
     expect(pool.getConfig()).to.haveOwnProperty('max')
-    expect(typeDetect(pool.getConfig().max)).to.equal(enumsTypeDetect.NUMBER)
+    expect(TypeDetect(pool.getConfig().max)).to.equal(EnumsTypeDetect.NUMBER)
     done()
   })
 
-  it ('Create First Pool Entry', (done) => {
-      service1 = pool.add(serviceEntry1)
-      expect(typeDetect(service1)).to.equal(enumsTypeDetect.PROMISE)
-      done()
+  it('Create First Pool Entry', (done) => {
+    service1 = pool.add(serviceEntry1)
+    expect(EnumsTypeDetect(service1)).to.equal(EnumsTypeDetect.PROMISE)
+    done()
   })
 
-  it ('Test Connection is Active', (done) => {
+  it('Test Connection is Active', (done) => {
     expect(pool.test(id1)).to.equal(true)
     done()
   })
 
-  it ('Check for 1 Active Service', (done) => {
+  it('Check for 1 Active Service', (done) => {
     expect(pool.getActiveServiceCount()).to.equal(1)
     done()
   })
 
-  it ('Destroy Connection', (done) => {
+  it('Destroy Connection', (done) => {
     expect(pool.destroy(id1)).to.equal(true)
     done()
   })
 
-  it ('Check for 0 Active Service', (done) => {
+  it('Check for 0 Active Service', (done) => {
     expect(pool.getActiveServiceCount()).to.equal(0)
     done()
   })
 
-  it ('Create 3 Pool Entries', (done) => {
+  it('Create 3 Pool Entries', (done) => {
     service1 = pool.add(serviceEntry1)
     service2 = pool.add(serviceEntry2)
     service3 = pool.add(serviceEntry3)
-      
-    expect(typeDetect(service1)).to.equal(enumsTypeDetect.PROMISE)
-    expect(typeDetect(service2)).to.equal(enumsTypeDetect.PROMISE)
-    expect(typeDetect(service3)).to.equal(enumsTypeDetect.PROMISE)
+
+    expect(TypeDetect(service1)).to.equal(EnumsTypeDetect.PROMISE)
+    expect(TypeDetect(service2)).to.equal(EnumsTypeDetect.PROMISE)
+    expect(TypeDetect(service3)).to.equal(EnumsTypeDetect.PROMISE)
     done()
   })
 
-  it (`Check for ${config.max} Active Service`, (done) => {
+  it(`Check for ${config.max} Active Service`, (done) => {
     expect(pool.getActiveServiceCount()).to.equal(config.max)
     done()
   })
 
-  it ('Add Duplicate Entry', (done) => {
+  it('Add Duplicate Entry', (done) => {
     service2 = pool.add(serviceEntry22)
-    expect(typeDetect(service2)).to.equal(enumsTypeDetect.PROMISE)
+    expect(TypeDetect(service2)).to.equal(EnumsTypeDetect.PROMISE)
     done()
   })
 
-  it ('Destroy 2nd Connection', (done) => {
+  it('Destroy 2nd Connection', (done) => {
     expect(pool.destroy(id3)).to.equal(true)
     done()
   })
 
-  it (`Check for ${config.max - 1} Active Service`, (done) => {
+  it(`Check for ${config.max - 1} Active Service`, (done) => {
     expect(pool.getActiveServiceCount()).to.equal(config.max - 1)
     done()
   })
 
-  it ('Test 1st Service is Active', (done) => {
+  it('Test 1st Service is Active', (done) => {
     expect(pool.test(id2)).to.equal(true)
     done()
   })
 
-  it ('Get 1st Service', (done) => {
-    expect(typeDetect(pool.get(id2))).to.equal(enumsTypeDetect.OBJECT)
+  it('Get 1st Service', (done) => {
+    expect(TypeDetect(pool.get(id2))).to.equal(EnumsTypeDetect.OBJECT)
     done()
   })
 
-  it ('Reset Services', (done) => {
+  it('Reset Services', (done) => {
     expect(pool.reset()).to.equal(true)
     done()
   })
 
-  it ('Check for 0 Active Service', (done) => {
+  it('Check for 0 Active Service', (done) => {
     expect(pool.getActiveServiceCount()).to.equal(0)
     done()
   })
